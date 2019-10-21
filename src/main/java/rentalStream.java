0eafcfec0311c772a3325e86758b10da976dd55b
Matt Miller <mattmiller0526@gmail.com>
2019-10-21 15:36:22 -0500

    Filled out Methods and Tested
    Methods work based on static arrays of data.  Next phase will be to switch the methods to point to Apollo for data rather than using hard coded data.

--- src/main/java/rentalStream.java
+++ src/main/java/rentalStream.java
@@ -1,7 +1,9 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+
 import com.datastax.dse.driver.api.core.DseSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
@@ -16,6 +18,8 @@ public class rentalStream {
         int number_of_rentals = 3;
 
         while (count < number_of_rentals) {
+            System.out.println("New Rental");
+            System.out.println("-----------");
             String rental_id = UUID.randomUUID().toString();
             String rental_start = LocalDateTime.now().toString();
             double start_fuel_level = 100;
@@ -25,18 +29,22 @@ public class rentalStream {
             String vehicle_id = getVehicle();
             String account_email = getAccountEmail();
 
-            int duration = 5;
-//            int duration = rand.nextInt(10 - 5); //Rentals will last at least 15 seconds but no more than 60 seconds
+//            int duration = 5;
+            int rental_duration = rand.nextInt(60 - 15); //Rentals will last at least 15 seconds but no more than 60 seconds
             int i = 0;
             int event_frequency_ms = 1000;
             double current_fuel_level = start_fuel_level;
 
             // While trip is happening, write the data to Apollo table 'live_trip'
-
-            while (i < duration){
+            while (i < rental_duration){
+                String time = LocalDateTime.now().toString();
                 double speed = generateSpeed();
                 current_fuel_level = generateFuelLevel(current_fuel_level);
-                System.out.println("Speed = "+speed+", Fuel = " +current_fuel_level+", Vehicle No: "+vehicle_id);
+                System.out.println("Speed = "+speed+
+                        ", Fuel = " +current_fuel_level+
+                        ", Vehicle No: "+vehicle_id+
+                        ", Time : "+time+
+                        ", Driver: " +account_email);
                 sleep(event_frequency_ms);
                 i++;
             }
@@ -44,23 +52,40 @@ public class rentalStream {
             //Once trip is done, complete the 'rental' record and write it to apollo
             end_location = getEndLocation(start_location);
             end_fuel_level = current_fuel_level;
-            String vehicle_type = getVehicleType();
+//            String vehicle_type = getVehicleType();
             String rental_stop = LocalDateTime.now().toString();
+            double miles_driven = i * 10.125;
 
-
+            System.out.println();
             System.out.println("End of rental.  See below for Rental Data:");
+//            System.out.println(
+//                    rental_id +','+
+//                    rental_start +','+
+//                    rental_stop +','+
+//                    vehicle_id +','+
+//                    vehicle_type+','+
+//                    start_fuel_level +','+
+//                    end_fuel_level +','+
+//                    start_location +','+
+//                    end_location +','+
+//                    account_email
+//            );
             System.out.println(
-                    rental_id +','+
-                    rental_start +','+
-                    rental_stop +','+
-                    vehicle_id +','+
-                    vehicle_type+','+
-                    start_fuel_level +','+
-                    end_fuel_level +','+
-                    start_location +','+
-                    end_location +','+
-                    account_email
+                            "Rental ID: " +rental_id +"\n"+
+                                    "Rental Start: " +rental_start +"\n"+
+                                    "Rental Stop: " +rental_stop +"\n"+
+                                    "Vehicle ID: " +vehicle_id +"\n"+
+//                                    "Vehicle Type: " +vehicle_type +"\n"+
+                                    "Starting Fuel Level: " +start_fuel_level +"\n"+
+                                    "Ending Fuel Level: " +end_fuel_level +"\n"+
+                                    "Start Location: " +start_location +"\n"+
+                                    "End Location: " +end_location +"\n"+
+                                    "Miles Driven :" +miles_driven +"\n"+
+                                    "Account: " +account_email
             );
+            System.out.println("--------------------------------------------");
+            System.out.println();
+            System.out.println();
             count ++;
         }
     }
@@ -103,27 +128,70 @@ public class rentalStream {
     }
 
     private static String getVehicle() {
-        String vehicle_id = "ct2019ls-0001";
         // code here
+        Random rand = new Random();
+        List<String> vehicle_id_list = Arrays.asList("ff2018lt-0001",
+                "fr2020st-0001",
+                "ct2019ls-0001",
+                "hc2016ss-0001",
+                "cp2019v-0001",
+                "tc2020s-0001",
+                "kf2018c-0001");
+        String vehicle_id = vehicle_id_list.get(rand.nextInt(7));
         return vehicle_id;
-    }
 
+    }
     private static String getStartLocation(){
-        String location = "SJC";
-        return location;
+        Random rand = new Random();
+        List<String> airport_list = Arrays.asList("SJC",
+                "SFO",
+                "San Francisco",
+                "San Jose",
+                "Mountain View",
+                "Cupertino",
+                "Palo Alto",
+                "Santa Clara",
+                "Santa Cruz",
+                "Los Gatos");
+        String airport_code = airport_list.get(rand.nextInt(10));
+        return airport_code;
     }
-
     private static String getEndLocation(String start_location) {
-        return start_location;
+        Random rand = new Random();
+        List<String> airport_list = Arrays.asList("SJC",
+                "SFO",
+                "San Francisco",
+                "San Jose",
+                "Mountain View",
+                "Cupertino",
+                "Palo Alto",
+                "Santa Clara",
+                "Santa Cruz",
+                "Los Gatos");
+        int chance = rand.nextInt(10);
+        String end_location = null;
+        // 10% chance of returning the car to a different airport
+        if(chance > 9){
+            end_location = airport_list.get(rand.nextInt(10));
+        }
+        else{
+            end_location = start_location;
+        }
+        return end_location;
     }
-
     private static String getAccountEmail() {
-        String email = "foo@aol.com";
+        Random rand = new Random();
+        List<String> account_list = Arrays.asList("matt@testemail.org'",
+                "foo@aol.com",
+                "sam@gmail.com'",
+                "ft@hotmail.com",
+                "jpn@yahoo.com",
+                "qanders@gmail.com",
+                "abogdon@outlook.com",
+                "smithers@gmail.com",
+                "kimmc@yahoo.com",
+                "sj@outlook.com");
+        String email = account_list.get(rand.nextInt(7));
         return email;
-    }
-
-    private static String getVehicleType() {
-        String vehicle_type = "Large SUV";
-        return vehicle_type;
     }
 }
